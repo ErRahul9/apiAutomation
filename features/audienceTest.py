a00590a7f09c3e9c41f66ed15b083aa93bdaf7af
Rahul Parashar <rahulparashar@Rahuls-MacBook-Pro.local>
2023-01-19 18:06:54 -0500

    updated code

--- features/audienceTest.py
+++ features/audienceTest.py
@@ -15,13 +15,26 @@ class audienceTest():
         print(method)
         if "get" in method:
             runMethod = getMethods(requrl,payload).getRequest()
+        if "put" in method:
+            runMethod = getMethods(requrl, payload).putRequest()
+        if "post" in method:
+            runMethod = getMethods(requrl, payload).postRequest()
+
         print(runMethod)
+        return runMethod
 
     def runsql(self):
         jsonObject = jsonReader("../features/fixtures", "job1.json").readJson()
         sql = jsonObject.get("audience").get("GET_LOCATION").get("sql")
         result = DatabaseConnection(sql).connectToPostgres()
         print(result)
+        return result
+
+    def compare(self):
+        requestResult = audienceTest().getRequest()
+        sqlresult = audienceTest().runsql
+
+
 
 if __name__=="__main__":
     audienceTest().getRequest()
