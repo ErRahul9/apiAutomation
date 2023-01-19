67fcca4a8f06734cb93acc5520d262ec3d2e713b
Rahul Parashar <rahulparashar@Rahuls-MacBook-Pro.local>
2023-01-19 18:00:47 -0500

    updated code to get sql

--- features/audienceTest.py
+++ features/audienceTest.py
@@ -1,6 +1,6 @@
 from helpers.jsonReader import jsonReader
 from helpers.getMethods import getMethods
-
+from helpers.databaseConn import DatabaseConnection
 
 class audienceTest():
 
@@ -17,7 +17,12 @@ class audienceTest():
             runMethod = getMethods(requrl,payload).getRequest()
         print(runMethod)
 
-
+    def runsql(self):
+        jsonObject = jsonReader("../features/fixtures", "job1.json").readJson()
+        sql = jsonObject.get("audience").get("GET_LOCATION").get("sql")
+        result = DatabaseConnection(sql).connectToPostgres()
+        print(result)
 
 if __name__=="__main__":
     audienceTest().getRequest()
+    audienceTest().runsql()
